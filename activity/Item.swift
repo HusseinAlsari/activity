69e3b1f1561a8adeb2e6fbac16402fc1c7c7bc92
HusseinAlsari <Hussainsori88@gmail.com>
2025-11-01 23:39:09 +0300

    Initial Commit

---
+++ activity/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  activity
+//
+//  Created by Hussein Alsari on 10/05/1447 AH.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ activity/activityApp.swift
@@ -0,0 +1,32 @@
+//
+//  activityApp.swift
+//  activity
+//
+//  Created by Hussein Alsari on 10/05/1447 AH.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct activityApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
