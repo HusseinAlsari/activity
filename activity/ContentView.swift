954fd23b9896c128facbec88afd782eee6aa799c
layana <layana.alhudaithi@gmail.com>
2025-11-02 15:46:07 +0300

    test

--- activity/ContentView.swift
+++ activity/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
 
     var body: some View {
         Text("Home")
+        Text("Helllllllllloooo")
         NavigationSplitView {
             List {
                 ForEach(items) { item in
