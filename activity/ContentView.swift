d8ea3456b430c454bdfee47e0b8c85e18a21b199
ailALQAHTANI <kfrfjqy42p@privaterelay.appleid.com>
2025-11-03 09:36:19 +0300

    هماسيب

--- activity/ContentView.swift
+++ activity/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
 
     var body: some View {
         Text("Home")
-        Text("Helllllllllloooo")
+        Text("Helllllllllممممممممممloooo")
         NavigationSplitView {
             List {
                 ForEach(items) { item in
